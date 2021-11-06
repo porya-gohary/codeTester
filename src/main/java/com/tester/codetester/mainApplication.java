6e3114f3d0a03f4413e3330e02401670f53f2b25
porya-gohary <gohary.porya@gmail.com>
2021-11-06 12:03:55 +0100

    minor improvement

--- src/main/java/com/tester/codetester/mainApplication.java
+++ src/main/java/com/tester/codetester/mainApplication.java
@@ -11,7 +11,7 @@ public class mainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(mainApplication.class.getResource("main-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 485, 485);
+        Scene scene = new Scene(fxmlLoader.load(), 485, 510);
         stage.setTitle("Code Tester");
         stage.setScene(scene);
         stage.show();
